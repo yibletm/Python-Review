9772034c8b8ac7f5bcab67489b249205c6f971de
Moses Yiblet <yibletm@gmail.com>
2026-05-25 21:27:38 -0400

    Add files via upload

---
+++ List and Loops.py
@@ -0,0 +1,14 @@
+fruits = ["apple, blueberry, strawberry, mangos"]
+print(fruits)
+
+i = 0
+
+while i<5:
+    if i == 3:
+        break
+    print("Hello people")
+    i=i+1
+
+for i in range(1,20, 2):
+    print(i)
+
