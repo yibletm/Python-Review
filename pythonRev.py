7717c3ba54ba6b02cbc2b81e9cd274ec6579a12b
Moses Yiblet <yibletm@gmail.com>
2026-06-16 21:31:44 -0400

    Add files via upload

---
+++ pythonRev.py
@@ -0,0 +1,45 @@
+# [] means list
+# () means tuple
+# {} means set or dictionary
+# dictionaries have {key:value} key value pairs in them
+
+# Error handling is "handled" primarily by try-except clauses
+print("What kind of 3 element list do you want to make today?")
+inpint = int(input("0 for list, 1 for tuple, 2, for set, or 3 for dictionary:"))
+
+if inpint == 0:
+    my_list = []
+    for i in range(3):
+        iter = i+1
+        elm = input(f"What should element {i} be?:")
+        my_list.append(elm)
+    struc = my_list
+else:
+    if inpint == 1:
+        my_list = []
+    for i in range(3):
+        iter = i+1
+        elm = input(f"What should element {i+1} be?:")
+        my_list.append(elm)
+        struc = tuple(my_list)
+    else:
+        if inpint == 2:
+            my_list = []
+        for i in range(3):
+            iter = i+1
+            elm = input(f"What should element {i+1} be?:")
+            my_list.append(elm)
+        struc = set(my_list)    
+print(struc)
+
+
+
+
+
+
+
+
+
+
+
+
