0e9f47155e65d34f807a8d7a5da7bfca769a2105
Moses Yiblet <yibletm@gmail.com>
2026-06-18 09:36:31 -0400

    Add files via upload

--- titanicDataPrep.py
+++ titanicDataPrep.py
@@ -11,12 +11,12 @@ titanic_df = sns.load_dataset('titanic')
 
 #next is for checking for any missing values
 
-print(titanic.describe())
+#print(titanic_df.describe())
 
 # Dealing with missing values 
 
 missing_values = titanic_df.isnull().sum()
-print(missing_values)
+#print(missing_values)
 
 # Dropping columns with excessive missing data
 new_titanic_df = titanic_df.drop(columns=['deck'])
@@ -26,16 +26,24 @@ new_titanic_df['age'] = new_titanic_df['age'].fillna(new_titanic_df['age'].media
 
 # Display the number of missing values post-imputation
 missing_values_updated = new_titanic_df.isnull().sum()
-print(missing_values_updated)
+#print(missing_values_updated)
 
-x = new_titanic_df.drop('Survived', axis=1) # drop removes the entirity of specifically the Survived column (while axis depicts how many columns)
+x = new_titanic_df.drop('survived', axis=1) # drop removes the entirity of specifically the Survived column (while axis depicts how many columns)
 #doesn't change data only applies them and assigns the changed set as x
 
-y = new_titanic_df['Survived']
+y = new_titanic_df['survived']
+print(y.shape)
 
 lb = LabelEncoder()
+x['sex'] = lb.fit_transform(x['sex'])
+x['embarked'] = lb.fit_transform(x['embarked'])
+x['class'] = lb.fit_transform(x['class'])
+x['who'] = lb.fit_transform(x['who'])
+x['adult_male'] = lb.fit_transform(x['adult_male'])
+x = x.drop('embark_town', axis=1)
+x['alive'] = lb.fit_transform(x['alive'])
+x['alone'] = lb.fit_transform(x['alone'])
 enc_y = lb.fit_transform(y) #fit_transform
-
 #after encoding, then you split the variables into 
 
 #the ordering for the different x and y values matters a whole lot.
@@ -43,8 +51,9 @@ enc_y = lb.fit_transform(y) #fit_transform
 x_train, x_test, y_train, y_test = train_test_split(x, enc_y, test_size=0.2, random_state=42)
 
 scaler = StandardScaler() #fit_transform applies standardization
+print(x.head())
 scal_x_train = scaler.fit_transform(x_train)
-scal_x_test = scaler.transform(x_test)
+scal_x_test = scaler.fit_transform(x_test)
 
 model = LogisticRegression(max_iter=200) #max_iter (iter means iteration) how many times it iterates through data
 model.fit(scal_x_train, y_train)
