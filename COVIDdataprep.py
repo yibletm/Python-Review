1fbe3fd640345ba3c68389ca5c473aa4721237c5
Moses Yiblet <yibletm@gmail.com>
2026-07-20 09:55:13 -0400

    Iris CV Made

--- COVIDdataprep.py
+++ COVIDdataprep.py
@@ -2,25 +2,38 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.metrics import accuracy_score
 import os
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+import warnings as wr
+wr.filterwarnings('ignore')
 ds = pd.read_csv("COVID19_Statistics_200_Rows-1.csv")
 missing_values = ds.isnull().sum()
 print(missing_values)
 ds = ds.drop_duplicates()
 ds.info()
 
-x = ds.drop('Vaccination_Rate_%', axis=1)
-y = ds['Vaccination_Rate_%']
+lb=LabelEncoder()
+
+ds['Date'] = lb.fit_transform(ds['Date'])
+ds['Country'] = lb.fit_transform(ds['Country'])
+ds['State'] = lb.fit_transform(ds['State'])
+
+corrmat = ds.corr(numeric_only=True)
+correlations=ds.corr()['Recovered'].abs().sort_values()
+Low_corr= correlations.head(3).index.tolist()
+print(corrmat)
+print(Low_corr)
+ds = ds.drop(columns=Low_corr)
+
+x = ds.drop('Recovered', axis=1)
+y = ds['Recovered']
 print(y.shape)
 print(x.head())
 
-lb=LabelEncoder()
 
-x['data'] = lb.fit_transform(x['data'])
-x['Country'] = lb.fit_transform(x['Country'])
-x['State'] = lb.fit_transform(x['State'])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=42)
 
@@ -29,22 +42,22 @@ print(x_train) #All values in x_train are numeric
 scal_x_train = scaler.fit_transform(x_train) #calculates the current mean and standard deviation
 scal_x_test = scaler.transform(x_test) #Calculates the previous mean and std dev
 
-model = LogisticRegression(max_iter=200) #max_iter (iter means iteration) how many times it iterates through data
+model = LinearRegression() #max_iter (iter means iteration) how many times it iterates through data
 model.fit(scal_x_train, y_train)
 y_pred = model.predict(scal_x_test)
-acc = accuracy_score(y_test, y_pred)
+acc = r2_score(y_test, y_pred)
 print(f"Logistic regression accuracy: {acc:.4f}")
 
-classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-classifier.fit(scal_x_train, y_train)
-y_pred = classifier.predict(scal_x_test)
-accuracy = accuracy_score(y_test, y_pred)
+regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+regressor.fit(scal_x_train, y_train)
+y_pred = regressor.predict(scal_x_test)
+accuracy = r2_score(y_test, y_pred)
 print(f'Random forest Accuracy: {accuracy * 100:.2f}%')
 
-print(ds.duplicated().sum())
+#print(ds.duplicated().sum())
 
-Train_acc= accuracy_score(y_train, model.predict(scal_x_train))
-Test_acc= accuracy_score(y_test, model.predict(scal_x_test))
+Train_acc= r2_score(y_train, model.predict(scal_x_train))
+Test_acc= r2_score(y_test, model.predict(scal_x_test))
 print(Train_acc)
 print(Test_acc)
 print(Train_acc-Test_acc)
---
+++ Iriscrossvalidate.py
@@ -0,0 +1,15 @@
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.svm import SVC
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X, y = iris.data, iris.target
+
+svm_classifier = SVC(kernel='linear')
+
+num_folds = 5
+kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
+
+cross_val_results = cross_val_score(svm_classifier, X, y, cv=kf)
+
+cross_val_results = cross_val_score(svm_classifier, X, y, cv=kf)
